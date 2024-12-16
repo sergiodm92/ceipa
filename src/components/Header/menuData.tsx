54002744f322f2b4c958ad23af9ebd43d7f1dcc4
sergiodm92 <crgiodm@gmail.com>
2024-12-16 16:26:15 -0300

    first commit

---
+++ src/components/Header/menuData.tsx
@@ -0,0 +1,35 @@
+import { Menu } from "@/types/menu";
+
+const menuData: Menu[] = [
+  {
+    id: 1,
+    title: "Inicio",
+    path: "/",
+    newTab: false,
+  },
+  {
+    id: 2,
+    title: "Empresas asociadas",
+    path: "/about",
+    newTab: false,
+  },
+  {
+    id: 33,
+    title: "Comision directiva",
+    path: "/blog",
+    newTab: false,
+  },
+  {
+    id: 3,
+    title: "Solicitud de ingreso",
+    path: "/contact",
+    newTab: false,
+  },
+  {
+    id: 4,
+    title: "Contacto",
+    path: "/contact",
+    newTab: false,
+  },
+];
+export default menuData;
